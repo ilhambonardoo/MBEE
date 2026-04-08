b980adf82f15a084557f74bc2dc88ff34222bdf8
ilhambonardoo <ilhambonardo123@gmail.com>
2026-04-08 20:53:30 +0700

    Buat API Route POST untuk register

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,25 @@
+import { register } from "@/src/app/controller/authController";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+
+    const response = await register(body);
+
+    if (response.status !== 201) {
+      return NextResponse.json(
+        { error: response.message },
+        { status: response.status },
+      );
+    }
+    return NextResponse.json({ message: response.message }, { status: 201 });
+  } catch {
+    return NextResponse.json(
+      {
+        message: "Terjadi kesalahan pada server",
+      },
+      { status: 500 },
+    );
+  }
+}
