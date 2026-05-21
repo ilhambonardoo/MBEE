b6f7524989144c87852a067e4c3da340b6a53803
ilhambonardoo <ilhambonardo123@gmail.com>
2026-05-21 21:27:26 +0700

    Membubat UserModel

--- src/interface/auth.ts
+++ src/interface/auth.ts
@@ -1,6 +1,17 @@
+import { Role } from "../generated/prisma/browser";
+
 export interface AuthModelRegister {
   nama: string;
   username: string;
   email: string;
   password: string;
+}
+
+export interface UserModel {
+  id: string;
+  nama: string;
+  username: string;
+  email: string;
+  password: string;
+  role: Role;
 }
