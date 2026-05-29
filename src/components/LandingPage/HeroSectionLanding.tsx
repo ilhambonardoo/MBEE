5418fd59259688ed0283b7066380fa56ff8da303
ilhambonardoo <ilhambonardo123@gmail.com>
2026-05-29 08:28:23 +0700

    Mengubah foto kambing menjadi video kambing

--- src/components/LandingPage/HeroSectionLanding.tsx
+++ src/components/LandingPage/HeroSectionLanding.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import Image from "next/image";
-import Link from "next/link";
+
 import { ArrowRight } from "lucide-react";
 import { useMounted } from "@/src/hooks/useMounted";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { signIn } from "next-auth/react";
+import { motion } from "framer-motion";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -83,42 +84,50 @@ const HeroSectionLanding = () => {
   return (
     <section
       ref={sectionRef}
-      className="relative bg-white w-full overflow-hidden min-h-screen flex flex-col items-center justify-center pt-32 pb-20"
+      className="relative bg-white w-full overflow-hidden min-h-screen flex flex-col items-center justify-center pt-20 md:pt-32"
     >
-      <div className="flex flex-col space-y-8 max-w-7xl px-8 pb-20 md:px-22 mx-auto z-20 w-full">
+      <div className="flex flex-col space-y-8 max-w-7xl px-6 md:px-22 mx-auto z-20 md:py-15 w-full">
         <h1
           ref={textRef}
-          className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-plenty font-medium tracking-tight text-gray-900 leading-[1.1] text-center md:text-left min-h-45 md:min-h-40 flex items-center"
+          className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-plenty font-medium tracking-tight text-gray-900 leading-[1.1] text-center md:text-left min-h-40 sm:min-h-50 md:min-h-40 flex items-center"
         >
           {finalTitle}
         </h1>
 
-        <div className="flex flex-col sm:flex-row items-center justify-center md:justify-start gap-4">
-          <Link
-            href="/login"
-            className="w-full sm:w-auto inline-flex items-center justify-center px-8 py-4 bg-amber-700 text-white font-semibold rounded-2xl hover:bg-amber-800 transition-all shadow-lg shadow-amber-200 group"
-          >
-            Mulai Sekarang
-            <ArrowRight className="ml-2 w-5 h-5 group-hover:translate-x-1 transition-transform" />
-          </Link>
-        </div>
+        <div className="hidden md:flex flex-col sm:flex-row items-center justify-center md:justify-start gap-4"></div>
       </div>
 
-      <div className="max-w-full mx-auto px-6 lg:px-0 relative z-10 mt-12 w-full flex justify-center">
+      <div className="max-w-full mx-auto px-4 lg:px-0 relative z-10 mt-8 md:mt-12 w-full flex justify-center">
         <div
           ref={imageWrapperRef}
-          className="relative overflow-hidden shadow-2xl origin-center rounded-3xl"
-          style={{ width: "85%" }}
+          className="relative overflow-hidden shadow-2xl origin-center rounded-2xl md:rounded-3xl w-[95%] md:w-[85%]"
         >
-          <div className="absolute inset-0 bg-linear-to-t from-black/20 to-transparent"></div>
-          <Image
-            src="/kambing.jpg"
-            alt="Smart Livestock Management"
-            width={1200}
-            height={800}
-            className="w-full h-auto object-cover aspect-video md:aspect-21/9"
-            priority
-          />
+          <div className="absolute inset-0 bg-linear-to-t from-black/40 via-transparent to-transparent z-10"></div>
+
+          <div className="absolute bottom-8 lg:bottom-30 lg:right-40 left-1/2 -translate-x-1/2 md:left-auto md:right-12 md:bottom-12 md:translate-x-0 flex items-center justify-center z-20 w-[calc(100%-2rem)] md:w-auto">
+            <motion.button
+              initial={{ opacity: 0 }}
+              whileHover={{ scale: 0.9, opacity: 0.6 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 1, ease: "easeInOut" }}
+              onClick={() => signIn()}
+              className="w-full md:w-auto bg-amber-600 cursor-pointer inline-flex items-center justify-center px-6 md:px-8 py-3 md:py-4 hover:bg-amber-700 text-white font-semibold rounded-xl md:rounded-2xl transition-all shadow-lg group whitespace-nowrap"
+            >
+              Masuk
+              <ArrowRight className="ml-2 w-4 h-4 md:w-5 md:h-5 transition-transform" />
+            </motion.button>
+          </div>
+          <video
+            className="w-full h-133 md:h-auto lg:h-screen object-cover aspect-video md:aspect-21/9"
+            autoPlay
+            muted
+            loop
+            playsInline
+            preload="auto"
+          >
+            <source src="/video/video-kambing.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
         </div>
       </div>
     </section>
